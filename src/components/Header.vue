cec50237161a5875140a13217636a0524aa537ae
Alireza <alirezafarmani619@gmail.com>
2025-01-27 11:38:07 -0800

    create and start project

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Container from '@/layouts/Container.vue';
+</script>
+
+<template>
+    <header class="w-full">
+        <Container>
+            <div class=""></div>
+            <div class=""></div>
+        </Container>
+    </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h2>home page</h2>
+</template>
